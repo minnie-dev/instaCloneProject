4273d6ef38441fe4d2bb3a869d15defcdd89d4e1
minhee <jung.dev98@gmail.com>
2022-10-11 10:40:42 +0900

    하울스타그램 알람 리스트 페이지 만들기

--- app/src/main/java/com/example/instaclone/navigation/AlarmFragment.kt
+++ app/src/main/java/com/example/instaclone/navigation/AlarmFragment.kt
@@ -5,20 +5,96 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.instaclone.R
 import com.example.instaclone.databinding.FragmentAlarmBinding
+import com.example.instaclone.databinding.ItemCommentBinding
+import com.example.instaclone.navigation.model.AlarmDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 
 class AlarmFragment : Fragment() {
-     private var _binding : FragmentAlarmBinding? = null
-     private val binding get() = _binding!!
+    private var _binding: FragmentAlarmBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentAlarmBinding.inflate(inflater,container,false)
-
+        _binding = FragmentAlarmBinding.inflate(inflater, container, false)
+        binding.alarmfragmentRecyclerview.adapter = AlarmRecyclerviewAdapter()
+        binding.alarmfragmentRecyclerview.layoutManager = LinearLayoutManager(activity)
         return binding.root
+    }
+
+    inner class AlarmRecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var alarmDTOList: ArrayList<AlarmDTO> = arrayListOf() //알람 저장하는 리스트 변수
+        lateinit var binding: ItemCommentBinding
+
+        init {
+            var uid = FirebaseAuth.getInstance().currentUser?.uid
+            FirebaseFirestore.getInstance() //나에게 도착한 메세지만 필터링
+                .collection("alarms")
+                .whereEqualTo("destinationUid", uid)
+                .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    alarmDTOList.clear()
+                    if (querySnapshot == null) return@addSnapshotListener
+
+                    for (snapshot in querySnapshot.documents) {
+                        alarmDTOList.add(snapshot.toObject(AlarmDTO::class.java)!!)
+                    }
+                    notifyDataSetChanged()
+                }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            binding = ItemCommentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return CustomViewHolder(binding)
+        }
+
+        inner class CustomViewHolder(binding: ItemCommentBinding) :
+            RecyclerView.ViewHolder(binding.root)
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            FirebaseFirestore.getInstance().collection("profilesImages")
+                .document(alarmDTOList[position].uid!!)
+                .get().addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val url = it.result!!["image"]
+                        Glide.with(binding.root.context)
+                            .load(url)
+                            .apply(RequestOptions().circleCrop())
+                            .into(binding.commentviewitemImageviewProfile)
+                    }
+                }
+            when (alarmDTOList[position].kind) {
+                0 -> {
+                    var str_0 = alarmDTOList[position].userId + getString(R.string.alarm_favorite)
+                    binding.commentviewitemTextviewProfile.text = str_0
+                }//좋아요 이벤트 알람
+                1 -> {
+                    var str_1 = alarmDTOList[position].userId + " " +
+                            getString(R.string.alarm_comment) + " of " + alarmDTOList[position].message
+                    binding.commentviewitemTextviewProfile.text = str_1
+                }//코멘트 이벤트 알람
+                2 -> {
+                    var str_2 =
+                        alarmDTOList[position].userId + " " + getString(R.string.alarm_follow)
+                    binding.commentviewitemTextviewProfile.text = str_2
+                }//팔로우 이벤트 알람
+            }
+            binding.commentviewitemTextviewComment.visibility = View.INVISIBLE
+        }
+
+        override fun getItemCount(): Int {
+            return alarmDTOList.size
+        }
+
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/instaclone/navigation/model/AlarmDTO.kt
+++ app/src/main/java/com/example/instaclone/navigation/model/AlarmDTO.kt
@@ -4,6 +4,9 @@ data class AlarmDTO (
     var destinationUid: String? = null,
     var userId: String? = null,
     var uid: String? = null,
+    //0 : like alarm
+    //1 : comment alarm
+    //2 : follow alarm
     var kind: Int? = null, // 어떤 타입의 메세지 종류
     var message: String? = null, // comment
     var timestamp: Long? = null
